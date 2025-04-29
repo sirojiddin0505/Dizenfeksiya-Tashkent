4de69d12af13fc915f2af2d5c185a601cd7ec2b7
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-04-29 15:52:47 +0500

    commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import './footer.css'
+import logo from '../images/logo.svg'
+
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <div className="container footer__container">
+        <div className="footer__left">
+          <img src={logo} alt="" className="footer__img" />
+        </div>
+        <div className="footer__right">
+        <span>
+            <p className="footer__text">Toshkent shahri  Yashnabod tumani  Mo’ljal Sergeli-4 bekat</p>
+          </span>
+          <span>
+            <p className="footer__text"><a target='_blank' href="tel:+998 94 099 3434" className="footer__link">+998 94 099 3434</a></p>
+          </span>
+          <hr />
+          <button className="footer__btn">Facebook</button>
+          <button className="footer__btn">Instagram</button>
+          <button className="footer__btn">Telegram</button>
+        </div>
+      </div>
+    </footer>
   )
 }
 
